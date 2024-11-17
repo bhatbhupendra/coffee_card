19a1045f041212434f4250a837b003080d630a32
Bhupendra Bhat <bhupendra420110bhat@gmail.com>
2024-11-17 23:18:58 +0545

    flutter crashcourse

---
+++ ytClassesHis/10/main.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+import 'package:coffee_card/home.dart';
+
+void main() {
+  runApp(const MaterialApp(
+    home: Home(),
+  ));
+}
---
+++ ytClassesHis/14/coffee_prefs.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+
+class CoffeePrefs extends StatelessWidget {
+  const CoffeePrefs({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(children: [
+      Row(children: [
+        const Text('Strength: '),
+        const Text('3'),
+        Image.asset(
+          'assets/img/coffee_bean.png',
+          width: 25,
+          colorBlendMode: BlendMode.multiply,
+          color: Colors.brown[100],
+        ),
+        const Expanded(child: SizedBox()),
+      ]),
+      Row(children: [
+        const Text('Sugars: '),
+        const Text('3'),
+        Image.asset(
+          'assets/img/sugar_cube.png',
+          width: 25,
+          colorBlendMode: BlendMode.multiply,
+          color: Colors.brown[100],
+        ),
+        const Expanded(child: SizedBox()),
+      ]),
+    ]);
+  }
+}
